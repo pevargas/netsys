612fc969f800973a8f5ef0cebc793845719144f2
Patrick Vargas <patrick.vargas@colorado.edu>
2013-09-17 17:39:37 -0600

    Added comments

--- gbn/GBNclient.c
+++ gbn/GBNclient.c
@@ -1,7 +1,14 @@
-/* GBNclient.c */
-/* This is a sample UDP client/sender using "sendto_.h" to simulate dropped packets.  */
-/* This code will not work unless modified. */
+////////////////////////////////////////////////////////////////////////////////
+// File: GBNclient.c                    Fall 2013
+// Students: 
+//   Grache Harsha                      Patrick Vargas
+//   b.grace.harsha@gmail.com           patrick.vargas@colorado.edu
+// University of Colorado Boulder       CSCI 4273: Network Systems
+// Requirements:
+//   This is a sample UDP client/sender using "sendto_.h" to simulate dropped packets.
+////////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////////////
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -15,38 +22,40 @@
 #include <signal.h>
 #include <unistd.h>
 #include "sendto_.h"
+////////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
-    
-	/* check command line args. */
-	if(argc<7)
-	{
-		printf("usage : %s <server_ip> <server_port> <error rate> <random seed> <send_file> <send_log> \n", argv[0]);
-		exit(1);
-	}
+  /* check command line args. */
+  if(argc<7)
+  {
+    printf("usage : %s <server_ip> <server_port> <error rate> <random seed> <send_file> <send_log> \n", argv[0]);
+    exit(1);
+  }
 
-	/* Note: you must initialize the network library first before calling sendto_().  The arguments are the <errorrate> and <random seed> */
-	init_net_lib(atof(argv[3]), atoi(argv[4]));
-	printf("error rate : %f\n",atof(argv[3]));
+  /* Note: you must initialize the network library first before calling sendto_().  The arguments are the <errorrate> and <random seed> */
+  init_net_lib(atof(argv[3]), atoi(argv[4]));
+  printf("error rate : %f\n",atof(argv[3]));
 
-	/* socket creation */
-	int sd;
-	if((sd = socket(**** CALL SOCKET() HERE TO CREATE A UDP SOCKET ****))<0)
-	{
-		printf("%s: cannot create socket \n",argv[0]);
-		exit(1);
-	}
+  /* socket creation */
+  int sd;
+  if((sd = socket(**** CALL SOCKET() HERE TO CREATE A UDP SOCKET ****))<0)
+  {
+    printf("%s: cannot create socket \n",argv[0]);
+    exit(1);
+  }
 
-	/* get server IP address (input must be IP address, not DNS name) */
-	struct sockaddr_in remoteServAddr;
-	bzero(&remoteServAddr,sizeof(remoteServAddr));               //zero the struct
-	remoteServAddr.sin_family = AF_INET;                 //address family
-	remoteServAddr.sin_port = htons(atoi(argv[2]));      //sets port to network byte order
-	remoteServAddr.sin_addr.s_addr = inet_addr(argv[1]); //sets remote IP address
-	printf("%s: sending data to '%s:%s' \n", argv[0], argv[1], argv[2]);
+  /* get server IP address (input must be IP address, not DNS name) */
+  struct sockaddr_in remoteServAddr;
+  bzero(&remoteServAddr,sizeof(remoteServAddr));               //zero the struct
+  remoteServAddr.sin_family = AF_INET;                 //address family
+  remoteServAddr.sin_port = htons(atoi(argv[2]));      //sets port to network byte order
+  remoteServAddr.sin_addr.s_addr = inet_addr(argv[1]); //sets remote IP address
+  printf("%s: sending data to '%s:%s' \n", argv[0], argv[1], argv[2]);
 
-	/* Call sendto_ in order to simulate dropped packets */
-	int nbytes;
-	char msg[] = "send this";
-	nbytes = sendto_(sd,msg, strlen(msg),0, (struct sockaddr *) &remoteServAddr, sizeof(remoteServAddr));
+  /* Call sendto_ in order to simulate dropped packets */
+  int nbytes;
+  char msg[] = "send this";
+  nbytes = sendto_(sd,msg, strlen(msg),0, (struct sockaddr *) &remoteServAddr, sizeof(remoteServAddr));
 }
+////////////////////////////////////////////////////////////////////////////////
