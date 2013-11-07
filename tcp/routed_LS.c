0b2106eb71236585528cc406fd9398ed884571e4
Patrick Vargas <patrick.vargas@colorado.edu>
2013-11-07 16:41:21 -0700

    Makin' my way down town.

--- tcp/routed_LS.c
+++ tcp/routed_LS.c
@@ -14,7 +14,7 @@
 //#include <netdb.h>
 //#include <netinet/in.h>
 //#include <signal.h>
-//#include <string.h>   /* memset() */
+#include <string.h>   /* memset(), strerror() */
 #include <stdio.h>
 #include <stdlib.h>
 //#include <sys/socket.h>
@@ -24,19 +24,46 @@
 //#include <time.h>
 
 #define ERROR( boolean ) if ( boolean ) {\
-  fprintf( stderr, "[%s:%i] %s\n", __FILE__, __LINE__-1, strerror( errno) );\
+  fprintf( stderr, "[%s:%i] %s\n", __FILE__, __LINE__-1, strerror( errno ) );\
   exit( EXIT_FAILURE );\
 }
+#define PKTSIZ 512
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+typedef struct {
+  char src;    // The name of the source router
+  char dst;    // The name of the destination router
+  int srcPort; // The port on source router (ie: Me)
+  int dstPort; // The port on the destination router
+  int cost;    // The cost to get there
+} Nodes;
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
 int main( int argc, char *argv[] ) {
+  // Variables
+  FILE *init;
+  char input[PKTSIZ];
+  Nodes LS[PKTSIZ];
 
   // Check command line args
   if( argc < 4 ) {
     printf("Usage: %s <RouterID> <LogFileName> <InitializationFile>\n", argv[0]);
     exit( EXIT_FAILURE );
   }
+
+  init = fopen( argv[3], "r" );
+  ERROR( init < 0 );
+
+  char *pch;
+  int count = 0;
+
+  while ( fgets( input, PKTSIZ, init ) != NULL ) {
+	printf( "%s", input );
+	
+  }
+  fclose( init );
 
   return EXIT_SUCCESS;
 }
